51ba40c24b34cf87833c124a504d7f6b88112e6e
Scheithauer, Volker <vscheith@trybmc.com>
2021-06-09 00:06:20 -0500

    update script

--- src/shell/remove_ctm_env.sh
+++ src/shell/remove_ctm_env.sh
@@ -1,51 +1,16 @@
 #!/usr/bin/env bash
-#================================================================
-# HEADER
-#================================================================
-#% SYNOPSIS
-#+    ${SCRIPT_NAME} [-hv] [pattern]
-#%
-#% DESCRIPTION
-#%    Remove environments of ctm aapi config
-#%
-#% OPTIONS
-#%    $1                            Control-M AAPI Environment pattern
-#%    -h, --help                    Print this help
-#%    -v, --version                 Print script information
-#%
-#% EXAMPLES
-#%    ${SCRIPT_NAME} [pattern]
-#%
-#================================================================
-#- IMPLEMENTATION
-#-    version         ${SCRIPT_NAME} (www.bmc.com.com) 0.0.1
-#-    author          Volker Scheithauer
-#-    copyright       Copyright (c) http://www.bmc.com
-#-    license         GPL-3.0-only or GPL-3.0-or-later
-#-    script_id       2021.07.00.00
-#-
-#================================================================
-#  HISTORY
-#     2011/06/008 : orchestrator : Script creation
-#
-#================================================================
-#  DEBUG OPTION
-#    set -n  # Uncomment to check your syntax, without execution.
-#    set -x  # Uncomment to debug this shell script
-#
-#================================================================
-# END_OF_HEADER
-#================================================================
-
 # set -x
+################################################################################
+# License                                                                         #
+################################################################################
 function license() {
         # On MAC update bash: https://scriptingosx.com/2019/02/install-bash-5-on-macos/
-
+        printf '%s\n' ""
         printf '%s\n' " GPL-3.0-only or GPL-3.0-or-later"
         printf '%s\n' " Copyright (c) 2021 BMC Software, Inc."
         printf '%s\n' " Author: Volker Scheithauer"
         printf '%s\n' " E-Mail: orchestrator@bmc.com"
-        printf '%s\n' " Contributor: Daniel Companeetz"        
+        printf '%s\n' " Contributor: Daniel Companeetz"
         printf '%s\n' ""
         printf '%s\n' " This program is free software: you can redistribute it and/or modify"
         printf '%s\n' " it under the terms of the GNU General Public License as published by"
@@ -61,6 +26,20 @@ function license() {
         printf '%s\n' " along with this program.  If not, see <https://www.gnu.org/licenses/>."
 }
 
+function ctm() {
+        printf '%s\n' ""
+        printf '%s\n' "  @@@@@@@   @@@@@@   @@@  @@@  @@@@@@@  @@@@@@@    @@@@@@   @@@                  @@@@@@@@@@   "
+        printf '%s\n' " @@@@@@@@  @@@@@@@@  @@@@ @@@  @@@@@@@  @@@@@@@@  @@@@@@@@  @@@                  @@@@@@@@@@@  "
+        printf '%s\n' " !@@       @@!  @@@  @@!@!@@@    @@!    @@!  @@@  @@!  @@@  @@!                  @@! @@! @@!  "
+        printf '%s\n' " !@!       !@!  @!@  !@!!@!@!    !@!    !@!  @!@  !@!  @!@  !@!                  !@! !@! !@!  "
+        printf '%s\n' " !@!       @!@  !@!  @!@ !!@!    @!!    @!@!!@!   @!@  !@!  @!!       @!@!@!@!@  @!! !!@ @!@  "
+        printf '%s\n' " !!!       !@!  !!!  !@!  !!!    !!!    !!@!@!    !@!  !!!  !!!       !!!@!@!!!  !@!   ! !@!  "
+        printf '%s\n' " :!!       !!:  !!!  !!:  !!!    !!:    !!: :!!   !!:  !!!  !!:                  !!:     !!:  "
+        printf '%s\n' " :!:       :!:  !:!  :!:  !:!    :!:    :!:  !:!  :!:  !:!   :!:                 :!:     :!:  "
+        printf '%s\n' "  ::: :::  ::::: ::   ::   ::     ::    ::   :::  ::::: ::   :: ::::             :::     ::   "
+        printf '%s\n' "  :: :: :   : :  :   ::    :      :      :   : :   : :  :   : :: : :              :      :    "
+}
+
 version="20.21.07.00"
 
 ################################################################################
@@ -68,19 +47,19 @@ version="20.21.07.00"
 ################################################################################
 function help() {
         # Display Help
-        echo "Remove environment definitions from ctm aaapi."
-        echo
-        echo "Syntax: remove_ctm_env.sh [-p|h|v]"
-        echo "options:"
-        echo "h     Print this Help."
-        echo "p     control-m aapi environment name search pattern."
-        echo "v     script version."
-        echo
+        printf '%s\n' ""
+        printf '%s\n' "Remove environment definitions from ctm aaapi."
+        printf '%s\n' ""
+        printf '%s\n' "Syntax: remove_ctm_env.sh [-p|h|v]"
+        printf '%s\n' "options:"
+        printf '%s\n' "h     Print this Help."
+        printf '%s\n' "p     control-m aapi environment name search pattern."
+        printf '%s\n' "v     script version."
         exit 0
 }
 
 function updateCtm() {
-        printf '%s\n' "Updating ctm environment"
+        printf '%s\n' " Updating ctm environment"
         echo '{' >ctmenv.deleted.json
         if [ "$pattern" != "" ]; then
                 todelete=$pattern
@@ -92,7 +71,7 @@ function updateCtm() {
                 # Check if there is a ctm envirnoment at all
                 num=$(cat ctmenv.current.json | wc -l)
                 if [ $num -eq 1 ]; then
-                        printf '%s\n' "Currently no ctm environment configured"
+                        printf '%s\n' " Currently no ctm environment configured"
                         echo "    \"counter\": 0," >>ctmenv.deleted.json
                         echo "    \"status\":\"no environment defined\"," >>ctmenv.deleted.json
                         echo "    \"pattern\":\"$pattern\"," >>ctmenv.deleted.json
@@ -105,8 +84,8 @@ function updateCtm() {
                 # Check if there is a ctm environment matching the search pattern
                 mapfile -t arr < <(jq -r 'keys[]' ctmenv.current.json)
                 if [ ${#arr[@]} -eq 0 ]; then
-                        printf '%s\n' "No ctm environment matching the pattern found"
-                        printf '%s\n' " - Search pattern: $pattern"
+                        printf '%s\n' " No ctm environment matching the pattern found"
+                        printf '%s\n' "  - Search pattern: $pattern"
                         echo "    \"counter\": 0," >>ctmenv.deleted.json
                         echo "    \"status\":\"no matching pattern\"," >>ctmenv.deleted.json
                         echo "    \"exit\": 2" >>ctmenv.deleted.json
@@ -118,8 +97,8 @@ function updateCtm() {
                         # Delete ctm environment matching the pattern
                         for item in $(printf "%s\n" ${arr[@]} | grep $todelete); do
                                 counter=$((counter + 1))
-                                echo "Found ctm environment matching the pattern"
-                                echo " - Deleting ctm environment $item"
+                                echo " Found ctm environment matching the pattern"
+                                echo "  - Deleting ctm environment $item"
                                 printf "\"$item\"," >>ctmenv.deleted.json
                                 status=$(ctm env delete $item)
                         done
@@ -145,7 +124,7 @@ function updateCtm() {
 
         else
                 # no parameter provided
-                printf '%s\n' "No pattern provided"
+                printf '%s\n' " No pattern provided"
                 echo "    \"counter\": 0," >>ctmenv.deleted.json
                 echo "    \"status\":\"no parameter provided\"," >>ctmenv.deleted.json
                 echo "    \"pattern\":\"\"," >>ctmenv.deleted.json
@@ -165,24 +144,31 @@ while [[ $# -gt 0 ]]; do
                 pattern="$2"
                 shift # past argument
                 shift # past value
+                ctm
                 updateCtm $pattern
 
                 ;;
         -h | --help)
-                HELP="$2"
-                shift # past argument
-                shift # past value
+                ctm
                 help
                 ;;
         -v | --version)
+                ctm
                 echo "Version: ${version}"
                 exit
                 ;;
         -l | --license)
+                ctm
                 license
                 exit
                 ;;
-               
+        *)
+                ctm
+                exit
+                ;;
+
         esac
 done
+ctm
 set -- "${POSITIONAL[@]}" # restore positional parameters
+exit 99
