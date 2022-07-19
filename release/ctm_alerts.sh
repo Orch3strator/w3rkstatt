2d9d0bcc7d38334bdf0efd93a6c5db425b0397a5
vscheith <vscheith@trybmc.com>
2022-07-18 21:32:16 -0400

    alert update

--- release/ctm_alerts.sh
+++ release/ctm_alerts.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-ALERTS_FILE="/home/truewatch/.w3rkstatt/logs/alerts.log"
-sudo sh -c "echo '$*' >> '${ALERTS_FILE}'"
-sudo su - ctmem -c "/usr/bin/python3 /opt/ctm/ctm_alerts.py $*"
+
+ALERTS_FILE_LOG="/home/truewatch/.w3rkstatt/logs/alerts.log"
+ALERTS_FILE_PYTHON="/opt/bmcs/w3rkstatt/ctm_alerts.py"
+USER_NAME="truewatch"
+USER_SHELL="/bin/bash"
+
+sudo -i -u ${USER_NAME} ${USER_SHELL} -c "echo '$*' >> '${ALERTS_FILE_LOG}'"
+sudo -i -u ${USER_NAME} ${USER_SHELL} -c "/usr/bin/python3 ${ALERTS_FILE_PYTHON} $*"
